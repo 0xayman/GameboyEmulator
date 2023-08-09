65a496725fd86fbd62d497f82d9bcfd561cc62d9
0xayman <aymantarig17@gmail.com>
2023-08-09 11:48:39 +0200

    feat: add SDL2 and basic rendering

--- src/modules/emu.rs
+++ src/modules/emu.rs
@@ -3,11 +3,15 @@ use crate::modules::cart::Cart;
 use crate::modules::common;
 use crate::modules::cpu::CPU;
 use crate::modules::ram::RAM;
+use crate::modules::ui::UI;
+use std::sync::mpsc;
+use std::thread;
 
+#[derive(Clone, Copy)]
 pub struct Emu {
     paused: bool,
     running: bool,
-    die: bool,
+    pub die: bool,
     ticks: u64,
 }
 
@@ -21,18 +25,31 @@ impl Emu {
         }
     }
 
-    pub fn run(&mut self, rom_path: &str) {
-        let mut cart = Cart::new();
-        cart.load(rom_path);
-
+    pub fn run_cpu_thread(emu: &mut Emu, cart: &mut Cart, rx: mpsc::Receiver<String>) {
         let mut ram = RAM::new();
-        let mut bus = Bus::new(&mut cart, &mut ram);
+        let mut bus = Bus::new(cart, &mut ram);
         let mut cpu = CPU::new(&mut bus);
 
         cpu.init();
 
-        while self.running {
-            if self.paused {
+        // try to recieve message from UI thread, if no message, run the while loop
+        // if message, check if it's "die", if so, break the loop
+        loop {
+            match rx.try_recv() {
+                Ok(msg) => {
+                    if msg == "die" {
+                        emu.die = true;
+                        break;
+                    }
+                }
+                Err(_) => {}
+            }
+
+            if emu.die {
+                break;
+            }
+
+            if emu.paused {
                 common::delay(10);
                 continue;
             }
@@ -42,10 +59,26 @@ impl Emu {
                 return;
             }
 
-            self.ticks += 1;
+            emu.ticks += 1;
 
-            print!("\nTICKS: {:0X}\t", self.ticks);
+            print!("\nTICKS: {:0X}\t", emu.ticks);
         }
+    }
+
+    pub fn run(&mut self, rom_path: &str) {
+        let mut emu = Emu::new();
+        let mut cart = Cart::new();
+        cart.load(rom_path);
+
+        let (tx, rx) = mpsc::channel::<String>();
+
+        // Create thread to run cpu
+        let cpu_thread_handler =
+            thread::spawn(move || Self::run_cpu_thread(&mut emu, &mut cart, rx));
+
+        UI::init(tx);
+
+        cpu_thread_handler.join().unwrap();
 
         return;
     }
--- src/modules/ui.rs
+++ src/modules/ui.rs
@@ -1,13 +1,51 @@
 pub mod UI {
+    use std::sync::mpsc;
 
-    const SCREEN_WIDTH: usize = 1024;
-    const SCREEN_HEIGHT: usize = 768;
+    use sdl2::{event::Event, Sdl};
 
-    pub fn init() {
-        //
-    }
+    use crate::modules::emu::Emu;
 
-    pub fn handle_events() {
-        //
+    const SCREEN_WIDTH: u32 = 1024;
+    const SCREEN_HEIGHT: u32 = 768;
+
+    pub fn init(tx: mpsc::Sender<String>) {
+        let sdl_context = sdl2::init().unwrap();
+        let video_subsystem = sdl_context.video().unwrap();
+        println!("SDL INIT");
+
+        let ttf_context = sdl2::ttf::init().unwrap();
+        println!("TTF INIT");
+
+        let window = video_subsystem
+            .window("Gameboy Emulator", SCREEN_WIDTH, SCREEN_HEIGHT)
+            .position_centered()
+            .build()
+            .unwrap();
+
+        let mut canvas = window.into_canvas().build().unwrap();
+
+        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.present();
+
+        let mut event_pump = sdl_context.event_pump().unwrap();
+
+        // loop
+        'running: loop {
+            canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+            canvas.clear();
+            canvas.present();
+            for event in event_pump.poll_iter() {
+                match event {
+                    sdl2::event::Event::Quit { .. } => {
+                        tx.send("die".to_string()).unwrap();
+                        break 'running;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        println!("SDL QUIT");
     }
 }
